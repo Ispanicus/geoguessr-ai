84c29acf80e5998d3a4680cdf3ff70e241376059
Johan Laursen <johan7laursen@gmail.com>
2022-03-04 11:04:55 +0100

    update countrydata.py

--- CountryDataTest.py
+++ CountryDataTest.py
@@ -23,10 +23,16 @@ except:
     print("country_dataset directory already exists")
 for file in metadata.keys():
     idx += 1
+    if f"country_data{jdx}.pickle" in os.listdir(country_data):
+        print(f"found country_data{jdx}, so continuing")
+        if idx == 10:
+            idx = 0
+            jdx += 1
+        continue
     try: 
         lat = metadata[file]["lat"]
         long = metadata[file]["lon"]
-        location = geolocator.reverse((lat, long),language='en')
+        location = geolocator.reverse((lat, long),language='en,*-Latn')
         address = location.raw['address']
     #     country = address.get('country', '')
         file_metadata[file] = address
@@ -35,10 +41,11 @@ for file in metadata.keys():
     
     if idx == 10:
         idx = 0
-        jdx += 1
+        
         with open(f"country_datatest/country_datatest{jdx}.pickle", 'wb') as handle:
             pickle.dump(file_metadata, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    if jdx == 2:
+        jdx += 1
+    if jdx == 3:
         break
 with open("country_datatest/country_datatest_final.pickle", 'wb') as handle:
     pickle.dump(file_metadata, handle, protocol=pickle.HIGHEST_PROTOCOL) 
