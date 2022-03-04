f4d2517be5dc4a3d83e2463f5146e73884bba58f
Johan Laursen <johan7laursen@gmail.com>
2022-03-04 10:23:14 +0100

    add countrydata scripts

---
+++ CountryDataTest.py
@@ -0,0 +1,37 @@
+import pickle
+import json
+import sys
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="geoapiExercises")
+idx = jdx = 0 
+file_metadata = {}
+try:
+    with open(sys.argv[1]) as file:
+        metadata = json.load(file)
+except:
+    print("Failed to open metadata file, make sure to pass as first argument. Current first argument is: ", )
+    try:
+        print(sys.argv[1])
+    except:
+        print("Failed to get first argument")
+for file in tqdm(metadata.keys()):
+    idx += 1
+    try: 
+        lat = metadata[file]["lat"]
+        long = metadata[file]["lon"]
+        location = geolocator.reverse((lat, long))
+        address = location.raw['address']
+    #     country = address.get('country', '')
+        file_metadata[file] = address
+    except:
+        file_metadata[file] = "Error"
+    
+    if idx == 10:
+        idx = 0
+        jdx += 1
+        with open(f"country_datatest/country_datatest{jdx}.pickle", 'wb') as handle:
+            pickle.dump(country_guess, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if jdx == 2:
+        break
+with open("country_datatest/country_datatest_final.pickle", 'wb') as handle:
+    pickle.dump(country_guess, handle, protocol=pickle.HIGHEST_PROTOCOL) 
